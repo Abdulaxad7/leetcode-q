4ee72ed31e222c8e8dac65b50f0e16251cd59ff4
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2024-06-25 12:15:56 +0500

    Problem 7

--- Leetcode/Q7.go
+++ Leetcode/Q7.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	reverseWords("asfdsa")
-
+	fmt.Println(findMedianSortedArrays([]int{1, 2, 3, 4, 7}, []int{5, 6, 7, 8}))
 }
-func reverseWords(s string) string {
-	var stri [][]string
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var joint []int
+	var result float64
 
-	for _, str := range s {
-		if str == ' ' {
+	joint = nums1
+	for _, v := range nums2 {
+		joint = append(joint, v)
 
-		}
-		stri[] = append(stri[], string(str))
 	}
-	fmt.Print(stri)
-	return s
+	for _, v := range joint {
+		result += float64(v)
+	}
+
+	sort.Ints(joint)
+
+	if len(joint)%2 == 0 {
+		result = float64(joint[len(joint)/2]+joint[len(joint)/2-1]) / 2
+
+	} else {
+		result = float64(joint[len(joint)/2])
+	}
+
+	return result
 }
